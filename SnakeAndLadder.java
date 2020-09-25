f371cb5ae2eea7408c66bddd81169cdb92b1d050
maqsood <maqsoodhamd37@gmail.com>
2020-09-25 14:01:58 +0530

    uc2 player rolls the die

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,8 +1,16 @@
 public class SnakeAndLadder{
-	static final int START_POSITION=0;
-	static final int FINAL_POSITION=100;
+	public static final int START_POSITION=0;
+	public static final int FINAL_POSITION=100;
+
+	public static int dieRoll(){
+		int number_on_die=(int)(Math.floor(Math.random()*6)+1.0);
+		return number_on_die;
+	}
+
 	public static void main(String[] args){
 		int player_position=START_POSITION;
+		int player_numberOnDie=SnakeAndLadder.dieRoll();
+		System.out.println("Player's number on die after die roll: "+player_numberOnDie);
 		System.out.println("Player's position: "+player_position);
 	}
 }
