053b27559d4a23ab2d4d627f85ed54a859b3b18a
maqsood <maqsoodhamd37@gmail.com>
2020-09-25 15:38:57 +0530

    uc6 reporting no. of times dice was played and position after each move

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -13,7 +13,6 @@ public class SnakeAndLadder{
 	public static int move(int player_position){
 		int player_numberOnDie=SnakeAndLadder.dieRoll();
 		int player_option=(int)(Math.floor(Math.random()*10)%3);
-		System.out.println("Player's number on die after die roll: "+player_numberOnDie);
 		switch(player_option){
 			case LADDER:
 				if ((player_position+player_numberOnDie)<=FINAL_POSITION)
@@ -26,17 +25,18 @@ public class SnakeAndLadder{
 				player_position-=player_numberOnDie;
 				break;
 		}
-		System.out.println("Player's option (1:LADDER, 0:NO_PLAY, 2:SNAKE): "+player_option);
 		if(player_position<START_POSITION) player_position=START_POSITION;
 		return player_position; 
 	}
 
 	public static void main(String[] args){
 		int player_position=START_POSITION;
+		int number_of_dieRolls=0;
 		while (player_position<FINAL_POSITION){
 			player_position=SnakeAndLadder.move(player_position);
-			System.out.println("Player's position after move: "+player_position);
+			number_of_dieRolls++;
+			System.out.println("Player's position after die roll number "+number_of_dieRolls+": "+player_position);
 		} 
-		System.out.println("Player won. Final player position is "+player_position);
+		System.out.println("Total number of times the dice was played: "+number_of_dieRolls);
 	}
 }
