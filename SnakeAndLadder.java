f22dba43bc0cb6f3ab7b84a76ab0a7d6af584cf2
maqsood <maqsoodhamd37@gmail.com>
2020-09-25 13:47:51 +0530

    uc1 game played with single player at start position 0

---
+++ SnakeAndLadder.java
@@ -0,0 +1,8 @@
+public class SnakeAndLadder{
+	static final int START_POSITION=0;
+	static final int FINAL_POSITION=100;
+	public static void main(String[] args){
+		int player_position=START_POSITION;
+		System.out.println("Player's position: "+player_position);
+	}
+}
